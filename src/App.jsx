a66193a3fc666d9db72fd561d8ebbb8707797bd5
Yiim <ilyimyan@gmail.com>
2024-08-07 13:52:36 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react"
 
 
 function App() {
 
-  const [close, setClose] = useState(false);
-
-  const handleClose = () => {
-    setClose(c => !c)
-  }
 
   return (
     <>
-    <div className="flex justify-center items-center h-[100vh]">
-    <div className={!close ? " w-[500px] h-[500px] bg-slate-600 relative rounded-lg" : " w-[500px] h-[500px] bg-slate-600 relative rounded-lg hidden"}>
-      <div onClick={handleClose} className="absolute right-[-15px] top-[-25px] bg-red-500 p-3 rounded-md text-white cursor-pointer hover:bg-red-700">X</div>
-    </div>
-    </div>
+
     </>
   )
 }
