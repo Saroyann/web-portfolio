00950fc1df0a68b1f706cfa156283b3eabb8308c
Yiim <ilyimyan@gmail.com>
2024-08-07 20:23:05 +0800

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,53 @@ import hero from '../assets/profile.png'
 import izumi1 from '../assets/konata home.png'
 import izumi2 from '../assets/konata home 2.png'
 import { ReactTyped } from 'react-typed'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
     return (
         <>
             <div className='-z-0 max-w-[1240px]'>
                 <div className="pt-28">
-                    <div className='relative mx-auto flex md:flex-col justify-center items-center'>
-                        <div className="absolute bg-yellow-200 w-[300px] h-[300px] rounded-full"></div>
-                        <img className="absolute left-0 w-[190px]" src={izumi1} />
-                        <img className="absolute right-0 w-[190px] scale-x-[-1]" src={izumi2} />
-                        <img className="relative w-[290px]" src={hero} alt="Profile" />
-                    </div>
+                <motion.div
+            className='relative mx-auto flex md:flex-col justify-center items-center'
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            layout
+        >
+            <motion.div
+                className="absolute bg-yellow-200 w-[300px] h-[300px] rounded-full"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 1, ease: 'easeInOut' }}
+                layout
+            ></motion.div>
+            <motion.img
+                className="absolute left-0 w-[190px]"
+                src={izumi1}
+                initial={{ x: -100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.5, ease: 'easeInOut' }}
+                layout
+            />
+            <motion.img
+                className="absolute right-0 w-[190px] scale-x-[-1]"
+                src={izumi2}
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.5, ease: 'easeInOut' }}
+                layout
+            />
+            <motion.img
+                className="relative w-[290px]"
+                src={hero}
+                alt="Profile"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 1, delay: 1, ease: 'easeInOut' }}
+                layout
+            />
+        </motion.div>
                     <div className='text-center w-[280px] mx-auto mt-3'>
                         <h2 className='text-2xl font-medium'>Hello I am</h2>
                         <h1 className='text-3xl font-bold py-2'>William Saroyan (YIIM)</h1>
