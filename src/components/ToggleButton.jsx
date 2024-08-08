2d58dff546fa6228d2f0e75b9e8c93e625681353
Yiim <ilyimyan@gmail.com>
2024-08-08 13:52:09 +0800

    initial commit

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -6,11 +6,11 @@ const ToggleButton = () => {
     const clickHandler = () => setClick(c => !c)
     return (
         <div className='flex justify-center lg:hidden'>
-            <p className='pr-2'>light</p>
+            <p className='pr-2 md:text-2xl md:pr-4'>light</p>
             <div onClick={clickHandler} className={!click ? 'w-12 md:w-16 h-5 md:h-8 bg-white rounded-full' : 'w-12 h-5 md:w-16 md:h-8 bg-slate-400 rounded-full'}>
                 <div className={!click ? 'w-5 h-5 md:w-8 md:h-8 bg-yellow-300 rounded-full ease-in-out duration-500' : 'w-5 h-5 md:w-8 md:h-8 bg-blue-400 rounded-full translate-x-7 ease-in-out duration-500'}></div>
             </div>
-            <p className='pl-2'>dark</p>
+            <p className='pl-2 md:text-2xl md:pl-4'>dark</p>
         </div>
     )
 }
