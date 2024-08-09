eef1bb2e13ff731268e61df9f1ce61b3b35c65b3
Yiim <ilyimyan@gmail.com>
2024-08-09 13:13:21 +0800

    initial commit

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Skills = () => {
     return (
         <section id='skills' className='h-[100vh] pt-[4rem] md:pt-[5rem]'>
-            <h1 className='mt-5 text-3xl text-center font-bold'>My Skills</h1>
+            <h1 className='mt-3 text-3xl text-center font-bold'>My Skills</h1>
+            <h2 className='text-center text-2xl font-medium mt-2'>bunch of my skills:</h2>
+
+            <div>
+                <div className='w-[300px] h-[300px] bg-slate-400 mx-auto my-4'></div>
+            </div>
         </section>
     )
 }
