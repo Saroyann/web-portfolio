91374dfed1da51c23001d855b894ba35b96241cc
Yiim <ilyimyan@gmail.com>
2024-08-10 11:30:08 +0800

    initial commit

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>
-
+        <section className='h-[100vh] pt-[35rem] md:pt-[4rem] relative' id='portfolio'>
+            <h1 className='mt-3 text-3xl md:text-6xl font-bold py-2 md:py-5 text-center'>Web Portfolio</h1>
+            <h2 className='text-center text-2xl md:text-4xl font-medium mt-2'>Web Portfolio Overview :</h2>
         </section>
     )
 }
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
     };
 
     return (
-        <section id='skills' className='h-[100vh] pt-[3rem] md:pt-[4rem] relative'>
+        <section id='skills' className='h-[100vh] overflow-y-scroll pt-[3rem] md:pt-[4rem]'>
 
             <div className={`fixed top-0 left-0 mx-auto w-full h-[100vh] backdrop-blur-lg z-20 ${visibleSkill ? '' : 'hidden'}`}>
                 <div className='flex justify-center items-center'>
@@ -75,20 +75,20 @@ const Skills = () => {
             <h2 className='text-center text-2xl md:text-4xl font-medium mt-2'>Skills Overview:</h2>
 
             <div className='md:grid md:grid-cols-2 md:pt-10'>
-                <div className='w-[300px] h-[330px] md:w-[340px] md:h-[390px] bg-slate-100 shadow-lg mx-auto my-6 rounded-xl'>
-                    <div className='w-full h-[200px] rounded-t-xl'>
-                        <img className='w-[12rem] mx-auto pt-3' src={webdev} />
+                <div className='w-[300px] h-[230px] md:w-[340px] md:h-[390px] bg-slate-100 shadow-lg mx-auto my-6 rounded-xl'>
+                    <div className='w-full h-[150px] rounded-t-xl'>
+                        <img className='w-[9rem] mx-auto pt-3' src={webdev} />
                     </div>
-                    <h3 className='text-center text-2xl md:text-4xl py-4'>Web Developer</h3>
-                    <div onClick={handleWebDeveloperClick} className='text-center bg-yellow-300 md:text-2xl rounded-lg w-fit mx-auto p-3 cursor-pointer shadow-md'>See Skills</div>
+                    <h3 className='text-center md:text-4xl'>Web Developer</h3>
+                    <div onClick={handleWebDeveloperClick} className='text-center bg-yellow-300 md:text-2xl rounded-lg w-fit mx-auto p-2 cursor-pointer shadow-md'>See Skills</div>
                 </div>
 
-                <div className='w-[300px] h-[330px] md:w-[340px] md:h-[390px] bg-slate-100 shadow-lg mx-auto my-6 rounded-xl'>
-                    <div className='w-full h-[200px] rounded-t-xl'>
-                        <img className='w-[6rem] mx-auto pt-14' src={pentab} alt="" />
+                <div className='w-[300px] h-[230px] md:w-[340px] md:h-[390px] bg-slate-100 shadow-lg mx-auto my-6 rounded-xl'>
+                    <div className='w-full h-[115px] rounded-t-xl'>
+                        <img className='w-[5rem] mx-auto pt-8' src={pentab} alt="" />
                     </div>
-                    <h3 className='text-center text-2xl md:text-4xl py-4'>Digital Artist</h3>
-                    <div onClick={handleDigitalArtistClick} className='text-center bg-yellow-300 md:text-2xl rounded-lg w-fit mx-auto p-3 cursor-pointer shadow-md'>See Skills</div>
+                    <h3 className='text-center md:text-4xl py-4'>Digital Artist</h3>
+                    <div onClick={handleDigitalArtistClick} className='text-center bg-yellow-300 md:text-2xl rounded-lg w-fit mx-auto p-2 cursor-pointer shadow-md'>See Skills</div>
                 </div>
 
                 <div className='w-[300px] h-[330px] md:w-[340px] md:h-[390px] bg-slate-100 shadow-lg mx-auto my-6 rounded-xl md:col-span-2'>
