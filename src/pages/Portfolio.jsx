a5eee357fac64752bc6b49a6625eae357338694b
Yiim <ilyimyan@gmail.com>
2024-08-10 14:26:25 +0800

    initial commit

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,11 +8,12 @@ const Portfolio = () => {
             <h1 className='mt-3 text-3xl md:text-6xl font-bold py-2 md:py-5 text-center'>Web Portfolio</h1>
             <h2 className='text-center text-2xl md:text-4xl font-medium my-2'>Bunch Of My Web Portfolio :</h2>
 
-            <div className={`pt-3 md:pt-10 overflow-y-scroll ${window.innerWidth < 768 ? 'h-[570px]' : ''}`}>
+            <div className={`pt-3 md:pt-0 overflow-y-scroll ${window.innerWidth < 768 ? 'h-[570px]' : ''}`}>
 
                 <div className='mx-auto w-fit md:gap-10 md:grid md:grid-cols-2'>
 
                     <div>
+                        <div className='text-2xl text-center pb-2 md:pt-7'>Kalkulator</div>
                         <img className='w-[290px] h-[345px] rounded-lg' src={calc} />
                         <a className='cursor-pointer' target='_blank' href="https://github.com/Saroyann/simple-calculator">
                             <div className='text-center mt-3 bg-yellow-400 md:text-2xl p-2 rounded-xl'>Source Code</div>
@@ -20,6 +21,7 @@ const Portfolio = () => {
                     </div>
 
                     <div>
+                        <div className='text-2xl text-center pt-4 pb-2'>ToDo List</div>
                         <img className='pt-4 w-[290px] h-[345px] rounded-lg' src={todolist} />
                         <a className='cursor-pointer' target='_blank' href="https://github.com/Saroyann/javascript-todolist">
                             <div className='text-center mt-3 bg-yellow-400 md:text-2xl p-2 rounded-xl'>Source Code</div>
@@ -27,6 +29,7 @@ const Portfolio = () => {
                     </div>
 
                     <div>
+                        <div className='text-2xl text-center pt-4 pb-2'>Cuaca</div>
                         <img className='pt-4 w-[290px] h-[345px] rounded-lg' src={weather} />
                         <a className='cursor-pointer' target='_blank' href="https://github.com/Saroyann/javascript-weather-app">
                             <div className='text-center mt-3 bg-yellow-400 md:text-2xl p-2 rounded-xl'>Source Code</div>
@@ -34,6 +37,7 @@ const Portfolio = () => {
                     </div>
 
                     <div>
+                        <div className='text-2xl text-center pt-4 pb-2'>QR Generator</div>
                         <img className='pt-4 w-[290px] h-[345px] rounded-lg' src={qr} />
                         <a className='cursor-pointer' target='_blank' href="https://github.com/Saroyann/javascript-qr-generator">
                             <div className='text-center mt-3 bg-yellow-400 md:text-2xl p-2 rounded-xl'>Source Code</div>
