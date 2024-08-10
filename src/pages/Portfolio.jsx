40e19f88a778c2a75d39c3551a3ac0d6d756fc2f
Yiim <ilyimyan@gmail.com>
2024-08-10 12:23:10 +0800

    initial commit

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,11 +4,11 @@ const Portfolio = () => {
     return (
         <section className='h-[100vh] pt-[2rem] md:pt-[4rem] relative' id='portfolio'>
             <h1 className='mt-3 text-3xl md:text-6xl font-bold py-2 md:py-5 text-center'>Web Portfolio</h1>
-            <h2 className='text-center text-2xl md:text-4xl font-medium my-2'>Bunch Of My Portfolio :</h2>
+            <h2 className='text-center text-2xl md:text-4xl font-medium my-2'>Bunch Of My Web Portfolio :</h2>
 
             <div className='mx-auto w-fit'>
                 <div className='w-[300px] h-[200px] bg-slate-200 rounded-lg'>
-
+                    <img src="" alt="" />
                 </div>
             </div>
         </section>
